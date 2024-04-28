c4592fe60b324cdc0dde17adfa4c911c3c4dbef1
ImAbuSayed <imabusayed@hotmail.com>
2024-04-28 13:04:52 +0600

    added comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<!-- This is a Blade template file for a Laravel URL Shortener web application. It includes HTML markup along with Blade templating syntax for dynamic content. The file contains sections for displaying a header with navigation links, a form to shorten URLs, and a section to display the shortened URL with a copy button functionality. Additionally, it includes JavaScript for clipboard functionality using ClipboardJS library. -->
+
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -7,7 +10,6 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
-
 <header class="container mx-auto p-6 bg-white rounded-lg shadow-md max-w-md">
     <h1 class="text-4xl font-bold text-blue-900 dark:text-blue-900">URL Shortener</h1>
     <nav>
@@ -42,7 +44,6 @@
             </button>
         </form>
     </section>
-
     <section class="result-section" id="shortened-url">
         <h3 class="text-lg font-bold text-gray-800 mb-2">Your shortened URL: @if(session ('shortedUrl')) <p id="short-link" class="rounded-md px-4 py-2 bg-gray-200 text-white-800">{{ url(session('shortedUrl')) }}</p> @endif</h3>
         <div class="flex items-center">
@@ -56,11 +57,9 @@
 <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
 <script>
     var clipboard = new ClipboardJS('#copy-button');
-
     clipboard.on('success', function(e) {
         // Optional: Display a success message or change button appearance
     });
-
     clipboard.on('error', function(e) {
         // Optional: Handle copy errors
     });
